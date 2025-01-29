2e54eed59584328f7885124b3ec2447c42ec8a8c
nov-bohdan <nov.bohdan@gmail.com>
2025-01-28 18:17:34 -0600

    All task progresses are created for new words regardless of user's choice

--- src/app/get_tasks_to_show/route.ts
+++ src/app/get_tasks_to_show/route.ts
@@ -2,6 +2,33 @@ import dbWords from "@/lib/db/words";
 import { Task } from "@/lib/practiceWords/types";
 import { TASK_TYPES } from "@/lib/practiceWords/tasks/TaskTypes";
 
+async function createTasksForWord(
+  wordTasks: {
+    id: number;
+    last_practiced: string | null;
+    progress_id: number;
+    score: number;
+    task_type: string;
+  }[],
+  wordProgressId: number
+) {
+  const savedTasks = [];
+  for (const taskType of TASK_TYPES) {
+    const matchingTask = wordTasks.find(
+      (currentTask) => currentTask.task_type === taskType
+    );
+    if (!matchingTask) {
+      // creating new user_task_progress
+      const newTask = await dbWords.createNewUserTaskProgress(
+        wordProgressId,
+        taskType
+      );
+      savedTasks.push(newTask);
+    }
+  }
+  return savedTasks;
+}
+
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export async function POST(request: Request): Promise<Response> {
   const taskTypesToUse: { task_types: (typeof TASK_TYPES)[number][] } =
@@ -10,28 +37,17 @@ export async function POST(request: Request): Promise<Response> {
   const wordProgresses = await dbWords.getWordsToPractice();
   for (const wordProgress of wordProgresses) {
     const wordTasks = await dbWords.getWordTaskProgresses(wordProgress.id);
+    const newTasks = await createTasksForWord(wordTasks, wordProgress.id);
+    wordTasks.push(newTasks);
     for (const taskType of taskTypesToUse.task_types) {
       const matchingTask = wordTasks.find(
         (currentTask) => currentTask.task_type === taskType
       );
-      if (!matchingTask) {
-        // creating new user_task_progress
-        const createdTask = await dbWords.createNewUserTaskProgress(
-          wordProgress.id,
-          taskType
-        );
-        tasksToShow.push({
-          ...createdTask,
-          word: wordProgress.words,
-          wordProgressId: wordProgress.id,
-        });
-      } else {
-        tasksToShow.push({
-          ...matchingTask,
-          word: wordProgress.words,
-          wordProgressId: wordProgress.id,
-        });
-      }
+      tasksToShow.push({
+        ...matchingTask,
+        word: wordProgress.words,
+        wordProgressId: wordProgress.id,
+      });
     }
   }
   return Response.json(tasksToShow);
